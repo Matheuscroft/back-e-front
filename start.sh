a06e2273e2728a2c3b90f64e0f948b31f6203fc7
Matheus OS <matheus.sdoliveira@gmail.com>
2024-12-21 02:07:56 -0300

    first commit

---
+++ start.sh
@@ -0,0 +1,7 @@
+docker-compose down
+
+docker build -t backend-bambu:latest ./nova_pasta
+
+docker build -t frontend-bambu:latest ./projeto do zero branco/novo-bambu
+
+docker-compose up --build --force-recreate --remove-orphans
